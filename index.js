58bc5c88c06fe1f437ed586236bbe5ce038a7518
Gul7333 <g.task.pk@gmail.com>
2024-11-04 22:42:49 +0500

    a

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
-// https://stackoverflow.com/a/76978444/383904
-const flipBook = (elBook) => {
-    elBook.style.setProperty("--c", 0); // Set current to first page
-    elBook.querySelectorAll(".page").forEach((page, i) => {
-        page.style.setProperty("--i", i);
-        page.addEventListener("click", (evt) => {
-            const c = !!evt.target.closest(".back") ? i : i + 1;
-            elBook.style.setProperty("--c", c);
-        });
-    });
+const mod = (n, m) => ((n % m) + m) % m; // Fix negative Modulo
+
+const elBook = document.querySelector(".book");
+const elsPages = elBook.querySelectorAll(".page");
+const tot = elsPages.length;
+
+console.log(tot);
+let c = 0; // Current page index
+
+const openPage = (index) => {
+  c = mod(index, tot + 1);
+  if(c === tot){
+    playSound("./sounds/end-flip.mp3")
+
+  }else{
+    playSound("./sounds/start-flip.mp3")
+  }
+
+  elBook.style.setProperty("--c", c);
 };
 
-document.querySelectorAll(".book").forEach(flipBook);
+elsPages.forEach((page, i) => {
+  page.style.setProperty("--i", i);
+  page.addEventListener("click", (evt) => {
+    c = !!evt.target.closest(".back") ? i : i + 1;
+    openPage(c);
+  });
+});
+
+function playSound(audioSrc) {
+    const audio = new Audio(audioSrc);
+    audio.play();
+  }
+addEventListener("keydown", (evt) => {
+    if (evt.key === "ArrowLeft" || evt.key === "ArrowRight") {
+    evt.preventDefault();
+    c += evt.key === "ArrowRight" ? +1 : -1;
+    openPage(c);
+
+  }
+});
+
+openPage(c);
+console.log(!!true)
